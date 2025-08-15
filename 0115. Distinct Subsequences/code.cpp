e38b6f5e2803767759c66f0cac350a7e8df90f89
Aditi Sonkar <imt_2023004@iiitm.ac.in>
2025-08-15 12:24:28 +0530

    updated

--- 0115. Distinct Subsequences/code.cpp
+++ 0115. Distinct Subsequences/code.cpp
@@ -1,12 +1,12 @@
 // tabulation
 class Solution {
 public:
-    const int prime = 1e9 + 7;
+    // const int prime = 1e9 + 7;
 
     int numDistinct(string s, string t) {
         int n = s.length();
         int m = t.length();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        vector<vector<double>> dp(n + 1, vector<double>(m + 1, 0));
 
         for (int i = 0; i <= n; i++)
             dp[i][0] = 1;
@@ -16,7 +16,7 @@ public:
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
                 if (s[i - 1] == t[j - 1])
-                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % prime;
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]);
 
                 else
                     dp[i][j] = dp[i - 1][j];
