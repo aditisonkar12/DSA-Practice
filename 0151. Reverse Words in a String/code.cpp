70c9b9d4ec3230995108494c69b48466b4e967af
Aditi Sonkar <imt_2023004@iiitm.ac.in>
2025-09-30 14:38:25 +0530

    updated

---
+++ 0151. Reverse Words in a String/code.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n=s.length();
+        string ans;
+        int i=0;
+        while(i<n){
+            while(i<n && s[i]==' ') i++;
+            if(i>=n) break;
+
+            int j=i+1;
+            while(j<n && s[j] !=' ') j++;
+            string word= s.substr(i,j-i);
+            if(ans.length()==0) ans=word;
+            else ans= word  + " " + ans;
+            i=j+1;
+        }
+        return ans;
+    }
+};
