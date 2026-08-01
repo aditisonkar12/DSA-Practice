b3196baadc9c319dc32c7fca864df98815f3c2d7
Aditi Sonkar <aditisonkar004@gmail.com>
2026-08-01 09:44:04 +0400

    Time: 522 ms (13.48%), Space: 329.1 MB (23.98%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
-    bool dfs(unordered_map<int, vector<int>>& adjList, int source,
-             int destination, vector<bool>& visited) {
-        if (source == destination)
-            return true;
-
-        if (visited[source] == true)
-            return false;
-        visited[source] = true;
-
-        for (auto& node : adjList[source]) {
-            if (dfs(adjList, node, destination, visited) == true)
-                return true;
-        }
-        return false;
-    }
-
     bool validPath(int n, vector<vector<int>>& edges, int source,
                    int destination) {
         unordered_map<int, vector<int>> adjList;
-        //building graph
         for (vector<int>& edge : edges) {
             int u = edge[0];
             int v = edge[1];
             adjList[u].push_back(v);
             adjList[v].push_back(u);
         }
+
         vector<bool> visited(n, false);
-        return dfs(adjList, source, destination, visited);
+        queue<int> q;
+        q.push(source);
+        visited[source]=true;
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            if (node == destination)
+                return true;
+
+            for (auto& ni : adjList[node]) {
+                if (!visited[ni]) {
+                    q.push(ni);
+                    visited[ni] = true;
+                }
+            }
+        }
+        return false;
     }
 };
