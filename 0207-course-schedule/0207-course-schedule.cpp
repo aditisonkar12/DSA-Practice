5be64a4bf4afdf78e4dc682dd4f88a68d8e7b284
Aditi Sonkar <aditisonkar004@gmail.com>
2026-08-05 09:13:07 +0400

    Time: 0 ms (100%), Space: 19 MB (90.6%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        int n = numCourses;
+        vector<vector<int>> adj(n);
+        vector<int> indegree(n, 0);
+
+        for (auto& e : prerequisites) {
+            int u = e[0];
+            int v = e[1];
+            adj[v].push_back(u);
+            indegree[u]++; // arrows goes from v->u so we increase indgegree of
+                           // u
+        }
+
+        queue<int> q;
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
+                cnt++;
+            }
+        }
+
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+
+            for (int& v : adj[u]) {
+                indegree[v]--;
+
+                if (indegree[v] == 0) {
+                    q.push(v);
+                    cnt++;
+                }
+            }
+        }
+        if (cnt == n)
+            return true;
+        return false;
+    }
+};
