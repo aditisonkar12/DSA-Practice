2a257631a2a5976fcd053a19e5f3bdbbaad5b19f
Aditi Sonkar <aditisonkar004@gmail.com>
2026-08-08 07:47:19 +0400

    Time: 0 ms (100%), Space: 8.4 MB (61.12%) - LeetHub

--- 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.cpp
+++ 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
     int largestInteger(int num) {
-        vector<int> digits;
-        int temp = num;
-        if (temp == 0)
-            digits.push_back(0);
+        string s = to_string(num);
+        priority_queue<int> even;
+        priority_queue<int> odd;
 
-        while (temp > 0) {
-            digits.push_back(temp % 10);
-            temp /= 10;
-        }
-
-        reverse(digits.begin(), digits.end());
-        vector<int> even, odd;
-
-        for (int d : digits) {
+        for (char ch : s) { // Store digits in heaps
+            int d = ch - '0';
             if (d % 2 == 0)
-                even.push_back(d);
+                even.push(d);
             else
-                odd.push_back(d);
+                odd.push(d);
         }
 
-        sort(even.begin(), even.end(), greater<int>());
-        sort(odd.begin(), odd.end(), greater<int>());
-
-        int e = 0, o = 0;
         int ans = 0;
-        for (int d : digits) {
-            if (d % 2 == 0)
-                ans = ans * 10 + even[e++];
-            else
-                ans = ans * 10 + odd[o++];
+        for (char ch : s) { // Rebuild number
+            if ((ch - '0') % 2 == 0) {
+                ans = ans * 10 + even.top();
+                even.pop();
+            } else {
+                ans = ans * 10 + odd.top();
+                odd.pop();
+            }
         }
         return ans;
     }
