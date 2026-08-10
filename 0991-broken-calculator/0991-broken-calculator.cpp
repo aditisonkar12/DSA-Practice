6afae705f24805aa90628fb277939dd20778aaf9
Aditi Sonkar <aditisonkar004@gmail.com>
2026-08-10 16:17:53 +0400

    Time: 0 ms (100%), Space: 7.9 MB (12.56%) - LeetHub

--- 0991-broken-calculator/0991-broken-calculator.cpp
+++ 0991-broken-calculator/0991-broken-calculator.cpp
@@ -7,6 +7,7 @@ public:
         if (target % 2 == 0) {
             return 1 + brokenCalc(startValue, target / 2);
         }
+        
         return 1 + brokenCalc(startValue, target + 1);
     }
 };
