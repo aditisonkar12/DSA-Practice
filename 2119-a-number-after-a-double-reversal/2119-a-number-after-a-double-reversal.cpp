dabce9b51957b74e494ba63c6b43d6b0e2141ef7
Aditi Sonkar <aditisonkar004@gmail.com>
2026-08-17 08:23:22 +0400

    Time: 0 ms (100%), Space: 7.7 MB (57.58%) - LeetHub

--- 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.cpp
@@ -1,8 +1,21 @@
 class Solution {
 public:
+    int reverseNum(int num) {
+        int rev = 0;
+        while (num > 0) {
+            rev = rev * 10 + num % 10;
+            num /= 10;
+        }
+        return rev;
+    }
+
     bool isSameAfterReversals(int num) {
-        if (num == 0)
-            return true;
-        return num % 10 != 0;
+        int temp = num;
+        num = reverseNum(num);
+        num = reverseNum(num);
+        return temp == num;
+        // if (num == 0)
+        //     return true;
+        // return num % 10 != 0;
     }
 };
