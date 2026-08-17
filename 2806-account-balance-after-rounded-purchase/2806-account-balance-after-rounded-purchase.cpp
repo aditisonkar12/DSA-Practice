db84a0eacaaabef602b6b21487baf126464bbb01
Aditi Sonkar <aditisonkar004@gmail.com>
2026-08-17 08:10:34 +0400

    Time: 0 ms (100%), Space: 7.8 MB (46.79%) - LeetHub

---
+++ 2806-account-balance-after-rounded-purchase/2806-account-balance-after-rounded-purchase.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int accountBalanceAfterPurchase(int purchaseAmount) {
+        int total = 100;
+        int roundedAmount = 0;
+        int rem = purchaseAmount % 10;
+        if (rem != 0) {
+            if (rem >= 5) {
+                roundedAmount = purchaseAmount + (10 - purchaseAmount % 10);
+            } else {
+                roundedAmount = purchaseAmount - rem;
+            }
+        }
+        else
+            roundedAmount = purchaseAmount;
+        return total - roundedAmount;
+    }
+};
