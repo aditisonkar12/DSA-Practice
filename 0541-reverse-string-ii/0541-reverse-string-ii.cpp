8806b30ed0a50a641076bf8ac5201fac47f18d29
Aditi Sonkar <aditisonkar004@gmail.com>
2026-08-20 07:38:44 +0400

    Time: 0 ms (100%), Space: 9.5 MB (77.3%) - LeetHub

---
+++ 0541-reverse-string-ii/0541-reverse-string-ii.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        int n = s.length();
+        for (int i = 0; i < n; i += 2 * k) {
+            int l = i;
+            int r = min(i + k - 1, n - 1);
+            reverse(s.begin() + l, s.begin() + r + 1);
+        }
+        return s;
+    }
+};
