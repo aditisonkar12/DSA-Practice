7ae265d85ab5da4bdc701604bc93a8e22e655729
Aditi Sonkar <aditisonkar004@gmail.com>
2026-08-27 20:17:53 +0400

    Time: 0 ms (100%), Space: 8.4 MB (75.08%) - LeetHub

---
+++ 2129-capitalize-the-title/2129-capitalize-the-title.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string capitalizeTitle(string title) {
+        // int n=title.length();
+        for (int i = 0; i < title.length(); i++) {
+            if (i == 0 || title[i - 1] == ' ') {
+                int j = i;
+                while (j < title.length() && title[j] != ' ') {
+                    j++;
+                }
+
+                int len = j - i;
+                if (len <= 2) {
+                    for (int k = i; k < j; k++) {
+                        title[k] = tolower(title[k]);
+                    }
+                } else {
+                    title[i] = toupper(title[i]);
+                    for (int k = i + 1; k < j; k++) {
+                        title[k] = tolower(title[k]);
+                    }
+                }
+            }
+        }
+        return title;
+    }
+};
